fcecdf97685ea43237147728cf474e58bccb40ce
OssianN <ossian.northen@gmail.com>
2021-10-23 16:07:30 +0200

    adds eyeposition change on character click

--- src/THREE/groups/CharacterGroup.js
+++ src/THREE/groups/CharacterGroup.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import { a, useSpring } from "@react-spring/three"
 import Body from "../meshs/character/Body"
 import Eyes from "../meshs/character/Eyes"
@@ -6,6 +6,7 @@ import Limb from "../meshs/character/Limb"
 
 const CharacterGroup = ({ mouseIsOver }) => {
   const [clicked, setClicked] = useState(false)
+  const [eyesPosition, setEyesPosition] = useState(-0.12)
 
   const limb1 = {
     v1: [-0.55, -0.2, 0],
@@ -31,19 +32,27 @@ const CharacterGroup = ({ mouseIsOver }) => {
     v3: [0.14, -1, 0],
   }
 
-  const eyesPosition = mouseIsOver ? -0.12 : 0
+  useEffect(() => {
+    setEyesPosition(mouseIsOver ? 0 : -0.12)
+  }, [mouseIsOver])
 
   const { rotation } = useSpring({
     rotation: clicked ? [0, 1.2, 0] : [0, 0.3, 0],
-    config: { mass: 1, tension: 10, friction: 1, precision: 0.1 },
+    config: { mass: 1, tension: 5, friction: 1, precision: 0.1 },
   })
 
   const handleClick = () => {
+    setEyesPosition(0.1)
     setClicked(true)
+
     setTimeout(() => {
       setClicked(false)
-    }, 500)
+    }, 700)
+    setTimeout(() => {
+      setEyesPosition(-0.12)
+    }, 1200)
   }
+
   return (
     <a.group
       position={[-1.9, -0.5, 12]}
